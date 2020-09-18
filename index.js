e6bad76c29942474815c39c4e10ed36445169d8c
MalikJ <malikjab01@gmail.com>
2020-09-18 19:45:58 -0400

    finished task1

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
 /************************************************************** Task 1: Warm-up! **************************************************************/
 //Task a: declare a variable called votingAge, console log true if age > 18 (no function required)
 
-
+const votingAge = 23;
+ if (votingAge > 18)
+ {
+    console.log(true)
+ }
 
 
 
 //Task b: declare a variable and then use a conditional to change the value of that variable based on the value assigned to a second variable (no function required)
 
+let testVar = 11;
+let testVar2 = 99;
+
+testVar = (testVar == testVar2) ? "25": "23";
+console.log(testVar)
 
 
 
 
 //Task c: Convert string ("1999") to integer (1999)  (no function required) // hint look up the Number method
 
-
-
+console.log(Number("1999"));
 
 
 //Task d: Write a function to multiply a*b 
 
-
+function multiply(a,b) {  
+    return a*b;
+  }
 
 
 
